c07ef46d98a3385f3dc3b3e4f8442a9ae9504043
bouchra07 <bouchra0805@gmail.com>
2021-01-24 17:50:30 +0100

    added frontend with word segmentation

--- src/app.py
+++ src/app.py
@@ -11,13 +11,17 @@ from DataLoader import DataLoader, Batch
 from Model import Model, DecoderType
 from SamplePreprocessor import preprocess
 import secrets
+import page
+import words
+from PIL import Image
+import cv2
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
 fnCharList = '../model/charList.txt'
 fnAccuracy = '../model/accuracy.txt'
-fnInfer = './static/uploads/test.png'
+fnInfer = './segmented/segment'
 
 app.config["UPLOAD_DIRECTORY"] = "static\\uploads"
 
@@ -39,20 +43,51 @@ def search():
     if request.method == "POST":
         RESULTS_ARRAY = []
         if request.files["image"]:
+
             parser = argparse.ArgumentParser()
             parser.add_argument('--dump', help='dump output of NN to CSV file(s)', action='store_true')
-
             args = parser.parse_args()
             decoderType = DecoderType.BestPath
+
             image = request.files["image"]
             image_dir_name = secrets.token_hex(16)
             image.save(os.path.join(app.config["UPLOAD_DIRECTORY"], image.filename))
-            print(image_dir_name)
-            print(open(fnAccuracy).read())
-            model = Model(open(fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
-            recognized = infer(model, fnInfer)
+            print(image.filename)
 
-            return render_template("index.html",image=image.filename, recognized=recognized)
+            image2 = cv2.cvtColor(cv2.imread("./static/uploads/"+image.filename), cv2.COLOR_BGR2RGB)
+            crop = page.detection(image2)
+            boxes = words.detection(crop)
+            lines = words.sort_words(boxes)
+
+            # Saving the bounded words from the page image in sorted way
+            i = 0
+            
+
+            open(fnAccuracy).read()
+            model = Model(open(fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
+            
+            for line in lines:
+                text = crop.copy()
+                for (x1, y1, x2, y2) in line:
+                    # roi = text[y1:y2, x1:x2]
+                    save = Image.fromarray(text[y1:y2, x1:x2])
+                    # print(i)
+                    save.save("segmented/segment" + str(i) + ".png")
+                    i += 1
+
+            path, dirs, files = next(os.walk("./segmented"))
+            file_count = len(files)
+
+            recognized = []
+            for i in range(file_count):
+                recognized.append(infer(model,"./segmented/segment" + str(i) + ".png"))
+
+
+            # print(open(fnAccuracy).read())
+            # model = Model(open(fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
+            # recognized = infer(model, fnInfer)
+
+            return render_template("index.html",image=image.filename, words=recognized)
         else:
             return render_template('index.html')
 
